b957c274976acd267494e6f4ff93cf193b9a84d8
Shivani Aggarwal <shivanisa33@gmail.com>
2019-01-19 08:24:38 -0500

    Updates

---
+++ precache-manifest.6b8fa82a84328ab771ab072add6972da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "85906be27e1c86cecdc1960ecefcc011",
+    "url": "/static/media/downArrow.85906be2.png"
+  },
+  {
+    "revision": "b2be88296a7e34923c22",
+    "url": "/static/css/main.4ba9e37f.chunk.css"
+  },
+  {
+    "revision": "e07dd8ee390670286a606e57e1fc6edb",
+    "url": "/static/media/todoey.e07dd8ee.png"
+  },
+  {
+    "revision": "3be8c2abde632b161a0d",
+    "url": "/static/js/1.3be8c2ab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a3e51376612a21b240d32f76be4f099",
+    "url": "/static/media/logo.2a3e5137.png"
+  },
+  {
+    "revision": "b2be88296a7e34923c22",
+    "url": "/static/js/main.b2be8829.chunk.js"
+  },
+  {
+    "revision": "fb531a5fb0b893e366a409ac0872ed2a",
+    "url": "/static/media/rightArrow.fb531a5f.png"
+  },
+  {
+    "revision": "6ae9f77fe64a5c67747cd494b0ede239",
+    "url": "/static/media/sotaro.6ae9f77f.png"
+  },
+  {
+    "revision": "e65f6f50360b0ea4c12820f55814945f",
+    "url": "/static/media/readykit.e65f6f50.png"
+  },
+  {
+    "revision": "3be8c2abde632b161a0d",
+    "url": "/static/css/1.8223ebe4.chunk.css"
+  },
+  {
+    "revision": "a1208eb356715e52b385ac5f548e421d",
+    "url": "/index.html"
+  }
+];
